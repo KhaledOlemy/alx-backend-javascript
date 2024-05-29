c50bcd3f01a62a4b750ee821a582586c975dbdce
khaledolemy <khaledolemy@gmail.com>
2024-05-30 02:27:22 +0300

    all in one

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => (
+      response.map((output) => ({
+        status: output.status,
+        value: output.status === 'fulfilled' ? output.value : String(output.reason),
+      }))
+    ));
+}

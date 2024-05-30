111a29a562c483ddae83a671c9b8eb9bfd50c171
khaledolemy <khaledolemy@gmail.com>
2024-05-30 22:41:27 +0300

    make classrooms

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,11 @@
+// export default class HolbertonCourse{
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   constructor(length) {
+//     this.length = length;
+//   }
+//   constructor(students) {
+//     this.students = students;
+//   }
+// }

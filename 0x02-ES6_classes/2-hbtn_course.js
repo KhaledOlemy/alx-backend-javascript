f262791b66d6ecbbcdcf933708b249b0234619e6
khaledolemy <khaledolemy@gmail.com>
2024-05-30 22:51:20 +0300

    setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,4 +4,37 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(clsName) {
+    if (typeof clsName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = clsName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(lengthValue) {
+    if (typeof lengthValue !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = lengthValue;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(studentArray) {
+    if (!Array.isArray(studentArray)) {
+      throw new TypeError('students must be an Array');
+    }
+    this._students = studentArray;
+  }
 }

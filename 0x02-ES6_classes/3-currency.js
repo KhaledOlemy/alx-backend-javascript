af8310885999192f6d8837b9ecc89dce5f4a0fee
khaledolemy <khaledolemy@gmail.com>
2024-05-30 22:58:09 +0300

    class with a method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(codeValue) {
+    if (typeof codeValue !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = codeValue;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nameValue) {
+    if (typeof nameValue !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = nameValue;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}

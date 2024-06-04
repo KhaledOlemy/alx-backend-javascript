be8c0b6da0bdf0ee1d5f2d780e411d88cc1d3273
khaledolemy <khaledolemy@gmail.com>
2024-06-04 09:28:39 +0300

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const outputMap = new Map();
+  outputMap.set('Apples', 10);
+  outputMap.set('Tomatoes', 10);
+  outputMap.set('Pasta', 1);
+  outputMap.set('Rice', 1);
+  outputMap.set('Banana', 5);
+  return outputMap;
+}

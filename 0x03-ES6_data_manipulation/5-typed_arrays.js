355d95a121c12772f3674948f1f9c558259172c7
khaledolemy <khaledolemy@gmail.com>
2024-06-04 07:57:27 +0300

    fix position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  buffer.setInt8(position, value);
+  buffer.setInt8(length-position, value);
   return buffer;
 }

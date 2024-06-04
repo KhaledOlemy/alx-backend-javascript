9d72a8530ac4ba30fa3d16a35a729e2fc61b07f7
khaledolemy <khaledolemy@gmail.com>
2024-06-04 09:15:25 +0300

    startsWith then join

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || !(typeof startString === 'string')) {
+    return '';
+  }
+  const outputArray = [];
+  for (const item of set.values()) {
+    if (item.startsWith(startString)) {
+      const subStr = item.substring(startString.length);
+      if (subStr) {
+        outputArray.push(subStr);
+      }
+    }
+  }
+  return outputArray.join('-');
+}

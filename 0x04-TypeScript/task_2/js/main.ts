560d72a085019f921e1251895981b28dcf64cba0
khaledolemy <khaledolemy@gmail.com>
2024-06-05 23:42:50 +0300

    Director vs Teacher

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,66 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface{
+
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+interface employeeInterface {
+  (salary: number): Director | Teacher
+}
+
+const createEmployee: employeeInterface = (salary: number): Director | Teacher => {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: Teacher | Director): boolean {
+  if (employee instanceof Director) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function executeWork(employee: Teacher | Director): void {
+  if (employee instanceof Director) {
+    employee.workDirectorTasks()
+  } else {
+    employee.workTeacherTasks()
+  }
+}

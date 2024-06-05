cfbdc3b016b4b6439f6d3cd893c87aace9be996a
khaledolemy <khaledolemy@gmail.com>
2024-06-06 01:13:35 +0300

    teacher and subjects | task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,18 @@
+namespace Subjects {
+  interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  class Cpp extends Subjects.Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC <= 0) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}

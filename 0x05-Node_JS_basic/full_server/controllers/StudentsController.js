bf1ee69e8be11be1d00d5585b813e74b673717ed
khaledolemy <khaledolemy@gmail.com>
2024-07-24 01:01:40 +0300

    FIX STUDENT CONTROLLER

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,24 +1,23 @@
 import readDatabase from '../utils.js';
 
 class StudentsController {
-  static getAllStudents(req, res) {
+  static async getAllStudents(req, res) {
     const databaseFile = process.argv[2];
 
-    readDatabase(databaseFile)
-      .then((data) => {
-        let responseText = 'This is the list of our students\n';
-        const fields = Object.keys(data).sort();
-        fields.forEach((field) => {
-          responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
-        });
-        res.status(200).send(responseText.trim());
-      })
-      .catch((error) => {
-        res.status(500).send(`Cannot load the database: ${error.message}`);
+    try {
+      const data = await readDatabase(databaseFile);
+      let responseText = 'This is the list of our students\n';
+      const fields = Object.keys(data).sort();
+      fields.forEach((field) => {
+        responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
       });
+      res.status(200).send(responseText.trim());
+    } catch (error) {
+      res.status(500).send(`Cannot load the database: ${error.message}`);
+    }
   }
 
-  static getAllStudentsByMajor(req, res) {
+  static async getAllStudentsByMajor(req, res) {
     const { major } = req.params;
     const databaseFile = process.argv[2];
 
@@ -27,18 +26,17 @@ class StudentsController {
       return;
     }
 
-    readDatabase(databaseFile)
-      .then((data) => {
-        if (!data[major]) {
-          res.status(404).send(`No students in the ${major} major`);
-        } else {
-          const studentList = data[major].join(', ');
-          res.status(200).send(`List: ${studentList}`);
-        }
-      })
-      .catch((error) => {
-        res.status(500).send(`Cannot load the database: ${error.message}`);
-      });
+    try {
+      const data = await readDatabase(databaseFile);
+      if (!data[major]) {
+        res.status(404).send(`No students in the ${major} major`);
+      } else {
+        const studentList = data[major].join(', ');
+        res.status(200).send(`List: ${studentList}`);
+      }
+    } catch (error) {
+      res.status(500).send(`Cannot load the database: ${error.message}`);
+    }
   }
 }
 

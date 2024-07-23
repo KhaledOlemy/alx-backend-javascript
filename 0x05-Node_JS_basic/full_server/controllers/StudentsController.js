a01f878b580f226852c33e5240b0b2feca0c3056
khaledolemy <khaledolemy@gmail.com>
2024-07-24 00:59:21 +0300

    FIX STUDENET CONTROLLER

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,15 +1,17 @@
-import readDatabase from '../utils/readDatabase.js';
+import readDatabase from '../utils.js';
 
 class StudentsController {
   static getAllStudents(req, res) {
-    readDatabase(process.argv[2])
+    const databaseFile = process.argv[2];
+
+    readDatabase(databaseFile)
       .then((data) => {
         let responseText = 'This is the list of our students\n';
-        const fields = Object.keys(data);
+        const fields = Object.keys(data).sort();
         fields.forEach((field) => {
           responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
         });
-        res.status(200).send(responseText);
+        res.status(200).send(responseText.trim());
       })
       .catch((error) => {
         res.status(500).send(`Cannot load the database: ${error.message}`);
@@ -18,10 +20,17 @@ class StudentsController {
 
   static getAllStudentsByMajor(req, res) {
     const { major } = req.params;
-    readDatabase(process.argv[2])
+    const databaseFile = process.argv[2];
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(400).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(databaseFile)
       .then((data) => {
         if (!data[major]) {
-          res.status(400).send('Major parameter must be CS or SWE');
+          res.status(404).send(`No students in the ${major} major`);
         } else {
           const studentList = data[major].join(', ');
           res.status(200).send(`List: ${studentList}`);
